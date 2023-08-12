669f4aa3669ce40f1847a1f7314974f9b116f83f
Hygor <Hygor@DESKTOP-55BA59J>
2023-08-12 16:16:50 -0300

    utilizando o metodo get para consumir o json e mostrar os dados na tela

--- Api_fake_JSON/src/App.jsx
+++ Api_fake_JSON/src/App.jsx
@@ -1,14 +1,40 @@
 import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react'
+import axios from 'axios';
 
 function App() {
   
-  
+  const [data, setDate] = useState([])
+
+  useEffect(()=>{
+    axios.get('http://localhost:3000/postagens')
+    .then(res => setDate(res.data))
+    .catch(err => console.log('Erro:',err))
+  }, [])
 
   return (
-   <>
-   </>
-  )
+  
+      <table>
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Autor</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((d, i)=> {
+            return <tr key={i}>
+              <td>{d.título}</td>
+              <td>{d.autor}</td>
+            </tr>
+          })}
+        </tbody>
+      </table>
+  );
 }
 
 export default App
+
+
+// Para rodar o json-server:json-server --watch ./mock/db.json
